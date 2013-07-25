24ddfa0e549a7c54913707f4de96e0abe65a38f3
Travis Swientek <travis@mailgunhq.com>
2013-07-25 14:07:06 -0700

    Fixed weird commit and merge issues

--- src/Mailgun/MailgunClient.php
+++ src/Mailgun/MailgunClient.php
@@ -2,48 +2,48 @@
 
 namespace Mailgun;
 
-use Mailgun\Connection\HttpBroker;
-use Mailgun\Unsubscribes\Unsubscribes;
+use Mailgun\Logs\Logs;
+use Mailgun\Stats\Stats;
+use Mailgun\Bounces\Bounces;
 use Mailgun\Messages\Messages;
 use Mailgun\Complaints\Complaints;
-use Mailgun\Bounces\Bounces;
-use Mailgun\Stats\Stats;
-use Mailgun\Logs\Logs;
-
-
-use Mailgun\Connection\Exceptions\NoDomainsConfigured;
-use Mailgun\Connection\Exceptions\InvalidCredentials;
-use Mailgun\Connection\Exceptions\MissingRequiredMIMEParameters;
-use Mailgun\Connection\Exceptions\GenericHTTPError;
+use Mailgun\Connection\HttpBroker;
+use Mailgun\Unsubscribes\Unsubscribes;
 
 class MailgunClient{
-
-	protected $debug;
-	protected $httpBroker;
-	
-	public function __construct($apiKey, $domain, $debug = false){
-		$this->httpBroker = new HttpBroker($apiKey, $domain, $debug);
-	}
-	
-	//Factory Methods for Class Creation from MailgunClient
-	public function Messages(){
-		return new Messages($this->httpBroker);
-	}
-	public function Unsubscribes(){
-		return new Unsubscribes($this->httpBroker);
-	}
-	public function Complaints(){
-		return new Complaints($this->httpBroker);
-	}
-	public function Bounces(){
-		return new Bounces($this->httpBroker);
-	}
-	public function Stats(){
-		return new Stats($this->httpBroker);
-	}
-	public function Logs(){
-		return new Logs($this->httpBroker);
-	}	
+    
+    /* 
+     * Instantiate the HttpBroker to make it available to all 
+     * classes created from here.
+    */
+    
+    public function __construct($apiKey, $domain, $debug = false){
+        $this->httpBroker = new HttpBroker($apiKey, $domain, $debug);
+    }
+    
+    /* 
+     * Factory methods for instantiating each endpoint class.
+     * If a new endpoint is added, create a factory method here.
+     */    
+     
+    public function Messages(){
+        return new Messages($this->httpBroker);
+    }
+    public function Unsubscribes(){
+        return new Unsubscribes($this->httpBroker);
+    }
+    public function Complaints(){
+        return new Complaints($this->httpBroker);
+    }
+    public function Bounces(){
+        return new Bounces($this->httpBroker);
+    }
+    public function Stats(){
+        return new Stats($this->httpBroker);
+    }
+    public function Logs(){
+        return new Logs($this->httpBroker);
+    }   
 }
 
 ?>
--- src/Mailgun/globals.php
+++ src/Mailgun/globals.php
@@ -7,7 +7,5 @@ const API_USER = "api";
 const SDK_VERSION = "0.1";
 const SDK_USER_AGENT = "mailgun-sdk-php";
 const DEFAULT_TIME_ZONE = "UTC";
-const DEFAULT_MG_API_KEY = "key-3ax6xnjp29jd6fds4gc373sgvjxteol0";
-const DEFAULT_MG_DOMAIN = "samples.mailgun.org";
 
 ?>
