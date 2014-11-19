b34805d86be355fc1252e6307a22d1761593f598
Travis Swientek <travis@mailgunhq.com>
2014-11-19 14:52:34 -0800

    Fixed broken test.

--- tests/Mailgun/Tests/MailgunTest.php
+++ tests/Mailgun/Tests/MailgunTest.php
@@ -16,21 +16,21 @@ class MailgunTest extends \Mailgun\Tests\MailgunTestCase
 
     public function testVerifyWebhookGood() {
         $client = new Mailgun('key-3ax6xnjp29jd6fds4gc373sgvjxteol0');
-        $postData = [
+        $postData = array(
             'timestamp' => '1403645220',
             'token' => '5egbgr1vjgqxtrnp65xfznchgdccwh5d6i09vijqi3whgowmn6',
             'signature' => '9cfc5c41582e51246e73c88d34db3af0a3a2692a76fbab81492842f000256d33',
-        ];
+        );
         assert($client->verifyWebhookSignature($postData));
     }
 
     public function testVerifyWebhookBad() {
         $client = new Mailgun('key-3ax6xnjp29jd6fds4gc373sgvjxteol0');
-        $postData = [
+        $postData = array(
             'timestamp' => '1403645220',
             'token' => 'owyldpe6nxhmrn78epljl6bj0orrki1u3d2v5e6cnlmmuox8jr',
             'signature' => '9cfc5c41582e51246e73c88d34db3af0a3a2692a76fbab81492842f000256d33',
-        ];
+        );
         assert(!$client->verifyWebhookSignature($postData));
     }
 }
