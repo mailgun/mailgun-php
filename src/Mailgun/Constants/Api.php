591f6a4538b5c7634686927a3d485afcaf850815
Michael Crawford <cerealean@me.com>
2014-11-20 12:28:39 -0600

    Constants.php  * Removed file
    Api.php
     * New class made for Api-related constants
    
    ExceptionMessages.php
     * New class made for Exception Messages constants
    
    Essentially, just moved the constants out into their own classes. Using classes so that it isn't necessary to do an include/require on any other files, just import the class.

---
+++ src/Mailgun/Constants/Api.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Mailgun\Constants;
+
+
+class Api {
+    const API_USER = "api";
+    const SDK_VERSION = "1.7";
+    const SDK_USER_AGENT = "mailgun-sdk-php";
+    const RECIPIENT_COUNT_LIMIT = 1000;
+    const CAMPAIGN_ID_LIMIT = 3;
+    const TAG_LIMIT = 3;
+    const DEFAULT_TIME_ZONE = "UTC";
+}
+ 
