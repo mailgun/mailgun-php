8fe342f5b992ca0b4f76b7be8c87c4bfdd96695a
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-09-18 09:56:14 +0200

    Apply fixes from StyleCI and make sure we add MIT license properly (#163)
    * Applied fixes from StyleCI
    
    * Create LICENSE

--- src/Mailgun/Mailgun.php
+++ src/Mailgun/Mailgun.php
@@ -1,5 +1,12 @@
 <?PHP
 
+/*
+ * Copyright (C) 2013-2016 Mailgun
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 namespace Mailgun;
 
 use Http\Client\HttpClient;
--- tests/Functional/SendMessageTest.php
+++ tests/Functional/SendMessageTest.php
@@ -1,9 +1,16 @@
 <?php
 
+/*
+ * Copyright (C) 2013-2016 Mailgun
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 namespace Mailgun\Tests\Functional;
 
 /**
- * Simple test to show how to use the MockedMailgun client
+ * Simple test to show how to use the MockedMailgun client.
  *
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  */
@@ -12,20 +19,20 @@ class SendMessageTest extends \PHPUnit_Framework_TestCase
     public function testSimpleExample()
     {
         // Create a Closure that validates the $files parameter to RestClient::send()
-        $fileValidator = function($files) {
-            $this->assertContains(['name'=>'from',    'contents'=>'bob@example.com'], $files);
-            $this->assertContains(['name'=>'to',      'contents'=>'alice@example.com'], $files);
-            $this->assertContains(['name'=>'subject', 'contents'=>'Foo'], $files);
-            $this->assertContains(['name'=>'text',    'contents'=>'Bar'], $files);
+        $fileValidator = function ($files) {
+            $this->assertContains(['name' => 'from',    'contents' => 'bob@example.com'], $files);
+            $this->assertContains(['name' => 'to',      'contents' => 'alice@example.com'], $files);
+            $this->assertContains(['name' => 'subject', 'contents' => 'Foo'], $files);
+            $this->assertContains(['name' => 'text',    'contents' => 'Bar'], $files);
         };
 
         // Create the mocked mailgun client. We use $this->assertEquals on $method, $uri and $body parameters.
         $mailgun = MockedMailgun::create($this, 'POST', 'domain/messages', [], $fileValidator);
 
-        $mailgun->sendMessage('domain', array(
+        $mailgun->sendMessage('domain', [
             'from'    => 'bob@example.com',
             'to'      => 'alice@example.com',
             'subject' => 'Foo',
-            'text'    => 'Bar'));
+            'text'    => 'Bar', ]);
     }
 }
