ac9dd246f5ca62eeebaddb55d0e6a4cb06d9f74b
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-11-24 07:28:09 +0100

    cs

--- src/Mailgun/Resource/Api/Domain/IndexResponse.php
+++ src/Mailgun/Resource/Api/Domain/IndexResponse.php
@@ -5,9 +5,7 @@
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
- *
  */
-
 
 namespace Mailgun\Resource\Api\Domain;
 
