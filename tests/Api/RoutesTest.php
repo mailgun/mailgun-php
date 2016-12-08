5249be9c599f09b513241763676237e5c6beace2
David Garcia <david.garcia@deralia.com>
2016-12-08 21:58:22 +0000

    Add initial (empty) Routes PHP Unit Test file
    We still need to provide the automated tests

---
+++ tests/Api/RoutesTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Copyright (C) 2013-2016 Mailgun
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+namespace Mailgun\Tests\Api;
+
+/**
+ * @author David Garcia <me@davidgarcia.cat>
+ */
+class RoutesTest extends TestCase
+{
+    protected function getApiClass()
+    {
+        return 'Mailgun\Api\Routes';
+    }
+}
