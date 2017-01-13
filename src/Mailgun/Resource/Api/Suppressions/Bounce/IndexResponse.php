c71946563b3b877bb0e6e46bd8950714bebda283
Sean Johnson <sean.johnson@maio.me>
2017-01-13 16:46:42 -0600

    Implement suppressions API (#258)

---
+++ src/Mailgun/Resource/Api/Suppressions/Bounce/IndexResponse.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * Copyright (C) 2013-2016 Mailgun
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+namespace Mailgun\Resource\Api\Suppressions\Bounce;
+
+use Mailgun\Resource\Api\PaginationResponse;
+use Mailgun\Resource\ApiResponse;
+
+/**
+ * @author Sean Johnson <sean@mailgun.com>
+ */
+class IndexResponse implements ApiResponse
+{
+    use PaginationResponse;
+
+    /**
+     * @var Bounce[]
+     */
+    private $items;
+
+    /**
+     * @param Bounce[] $items
+     * @param array    $paging
+     */
+    public function __construct(array $items, array $paging)
+    {
+        $this->items = $items;
+        $this->paging = $paging;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return IndexResponse
+     */
+    public static function create(array $data)
+    {
+        $bounces = [];
+        if (isset($data['items'])) {
+            foreach ($data['items'] as $item) {
+                $bounces[] = Bounce::create($item);
+            }
+        }
+
+        return new self($bounces, $data['paging']);
+    }
+
+    /**
+     * @return Bounce[]
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+}
