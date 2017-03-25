e74d7ad130e70f43c61a0a4e64971bd16d0a929f
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-03-25 14:02:11 +0100

    Added test for Route::Create (#306)
    * Added test for Route::Create
    
    * cd

--- tests/Api/RoutesTest.php
+++ tests/Api/RoutesTest.php
@@ -9,6 +9,8 @@
 
 namespace Mailgun\Tests\Api;
 
+use GuzzleHttp\Psr7\Response;
+
 /**
  * @author David Garcia <me@davidgarcia.cat>
  */
@@ -19,31 +21,21 @@ class RoutesTest extends TestCase
         $this->markTestSkipped('Routes API tests not implemented yet.');
     }
 
+    public function testCreate()
+    {
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('httpPost')
+            ->willReturn(new Response());
+
+        $api->create('catch_all()', ['forward("mailbox@myapp.com")'], 'example', 100);
+    }
+
     /**
      * {@inheritdoc}
      */
     protected function getApiClass()
     {
         return 'Mailgun\Api\Routes';
-    }
-
-    public function testGetRoutesCollection()
-    {
-    }
-
-    public function testGetRoutesResource()
-    {
-    }
-
-    public function testPostRouteResource()
-    {
-    }
-
-    public function testUpdateRouteResource()
-    {
-    }
-
-    public function testDeleteRouteResource()
-    {
     }
 }
