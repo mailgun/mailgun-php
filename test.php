cfe2424b604b982e9991834436030b42aa5d3e1c
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-04-08 10:34:40 +0200

    Delete test file.

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include "vendor/autoload.php";
-
-$mailgun = \Mailgun\Mailgun::create('key-f22961235cdf7aaef78a9228c710f12c');
-$bounces = $mailgun->suppressions()->bounces()->index('sandbox40c4b0745b8a4461959950ccf9c0a1ea.mailgun.org');
-$x = 2;
