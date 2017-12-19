ce2bd4b0f1cbf1730795c5ffd63bb7c966b5fbbf
Quentin <quentin.pautrat@gmail.com>
2017-12-19 11:24:03 +0100

    feat(unsubscribe): Handle tags in Unsubscribe v3 api.

---
+++ tests/Model/Suppression/Unsubscribe/UnsubscribeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Mailgun\Tests\Model\Suppression\Unsubscribe;
+
+use Mailgun\Model\Suppression\Unsubscribe\Unsubscribe;
+use PHPUnit_Framework_TestCase;
+
+class UnsubscribeTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_gets_empty_list_of_tags_by_default()
+    {
+        $unsubscribe = Unsubscribe::create(['address' => 'dummy@mailgun.net']);
+        $this->assertEquals([], $unsubscribe->getTags());
+    }
+
+    /**
+     * @test
+     */
+    public function it_gets_tags()
+    {
+        $tags = ['tag1', 'tag2'];
+        $unsubscribe = Unsubscribe::create(['address' => 'dummy@mailgun.net', 'tags' => $tags]);
+        $this->assertEquals($tags, $unsubscribe->getTags());
+    }
+}
