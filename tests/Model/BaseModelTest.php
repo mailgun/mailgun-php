1cd31da772df1cef94ad912f29586d8a6e747966
Nyholm <tobias.nyholm@gmail.com>
2018-08-05 12:44:59 +0200

    cs

--- tests/Model/BaseModelTest.php
+++ tests/Model/BaseModelTest.php
@@ -2,9 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * Copyright (C) 2013 Mailgun
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
 namespace Mailgun\Tests\Model;
 
 class BaseModelTest extends \PHPUnit_Framework_TestCase
 {
-
 }
--- tests/Model/Domain/ConnectionResponseTest.php
+++ tests/Model/Domain/ConnectionResponseTest.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+/*
+ * Copyright (C) 2013 Mailgun
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
 namespace Mailgun\Tests\Model\Domain;
 
 use Mailgun\Model\Domain\ConnectionResponse;
@@ -12,7 +19,7 @@ class ConnectionResponseTest extends BaseModelTest
     public function testCreate()
     {
         $json =
-<<<JSON
+<<<'JSON'
 {
   "connection": {
     "require_tls": false,
