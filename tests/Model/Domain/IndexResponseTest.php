0145ddd4815020931775bc734c54167801189aad
Nyholm <tobias.nyholm@gmail.com>
2018-08-05 13:04:39 +0200

    Added more tests

---
+++ tests/Model/Domain/IndexResponseTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (C) 2013 Mailgun
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+namespace Mailgun\Tests\Model\Domain;
+
+use Mailgun\Model\Domain\CreateResponse;
+use Mailgun\Model\Domain\DeleteCredentialResponse;
+use Mailgun\Model\Domain\DeleteResponse;
+use Mailgun\Model\Domain\DnsRecord;
+use Mailgun\Model\Domain\Domain;
+use Mailgun\Model\Domain\IndexResponse;
+use Mailgun\Tests\Model\BaseModelTest;
+
+class IndexResponseTest extends BaseModelTest
+{
+    public function testCreate()
+    {
+        $json =
+<<<'JSON'
+{
+  "total_count": 1,
+  "items": [
+    {
+      "created_at": "Wed, 10 Jul 2013 19:26:52 GMT",
+      "smtp_login": "postmaster@samples.mailgun.org",
+      "name": "samples.mailgun.org",
+      "smtp_password": "4rtqo4p6rrx9",
+      "wildcard": true,
+      "spam_action": "disabled",
+      "state": "active"
+    }
+  ]
+}
+
+JSON;
+        $model = IndexResponse::create(json_decode($json, true));
+        $this->assertEquals(1, $model->getTotalCount());
+        $this->assertCount(1, $model->getDomains());
+    }
+}
---
+++ tests/Model/Domain/ShowResponseTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (C) 2013 Mailgun
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+namespace Mailgun\Tests\Model\Domain;
+
+use Mailgun\Model\Domain\CreateResponse;
+use Mailgun\Model\Domain\DeleteCredentialResponse;
+use Mailgun\Model\Domain\DeleteResponse;
+use Mailgun\Model\Domain\DnsRecord;
+use Mailgun\Model\Domain\Domain;
+use Mailgun\Model\Domain\IndexResponse;
+use Mailgun\Model\Domain\ShowResponse;
+use Mailgun\Tests\Model\BaseModelTest;
+
+class ShowResponseTest extends BaseModelTest
+{
+    public function testCreate()
+    {
+        $json =
+<<<'JSON'
+{
+  "domain": {
+    "created_at": "Wed, 10 Jul 2013 19:26:52 GMT",
+    "smtp_login": "postmaster@domain.com",
+    "name": "domain.com",
+    "smtp_password": "4rtqo4p6rrx9",
+    "wildcard": false,
+    "spam_action": "tag",
+    "state": "active"
+  },
+  "receiving_dns_records": [
+    {
+      "priority": "10",
+      "record_type": "MX",
+      "valid": "valid",
+      "value": "mxa.mailgun.org"
+    },
+    {
+      "priority": "10",
+      "record_type": "MX",
+      "valid": "valid",
+      "value": "mxb.mailgun.org"
+    }
+  ],
+  "sending_dns_records": [
+    {
+      "record_type": "TXT",
+      "valid": "valid",
+      "name": "domain.com",
+      "value": "v=spf1 include:mailgun.org ~all"
+    },
+    {
+      "record_type": "TXT",
+      "valid": "valid",
+      "name": "domain.com",
+      "value": "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUA...."
+    },
+    {
+      "record_type": "CNAME",
+      "valid": "valid",
+      "name": "email.domain.com",
+      "value": "mailgun.org"
+    }
+  ]
+}
+JSON;
+        $model = ShowResponse::create(json_decode($json, true));
+        $this->assertCount(2, $model->getInboundDNSRecords());
+        $this->assertCount(3, $model->getOutboundDNSRecords());
+        $this->assertInstanceOf(Domain::class, $model->getDomain());
+    }
+}
