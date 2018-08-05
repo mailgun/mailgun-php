0c70fa1f0e735ee649bbbaedc4a11fc5440bb6f7
Nyholm <tobias.nyholm@gmail.com>
2018-08-05 13:30:36 +0200

    Make BaseModelTest abstract

--- tests/Model/BaseModelTest.php
+++ tests/Model/BaseModelTest.php
@@ -11,6 +11,6 @@ declare(strict_types=1);
 
 namespace Mailgun\Tests\Model;
 
-class BaseModelTest extends \PHPUnit_Framework_TestCase
+abstract class BaseModelTest extends \PHPUnit_Framework_TestCase
 {
 }
