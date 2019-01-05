e968edc45a879d1e66e14a00b4cbdbb192d1850b
Nyholm <tobias.nyholm@gmail.com>
2019-01-05 12:29:44 +0100

    cs

--- tests/Exception/HttpClientExceptionTest.php
+++ tests/Exception/HttpClientExceptionTest.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Copyright (C) 2013 Mailgun
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
 namespace Mailgun\Tests\Exception;
 
 use GuzzleHttp\Psr7\Response;
@@ -10,14 +17,14 @@ class HttpClientExceptionTest extends MailgunTestCase
 {
     public function testBadRequestGetMessageJson()
     {
-        $response = new Response(400, ['Content-Type'=>'application/json'], '{"message":"Server Message"}');
+        $response = new Response(400, ['Content-Type' => 'application/json'], '{"message":"Server Message"}');
         $exception = HttpClientException::badRequest($response);
         $this->assertStringEndsWith('Server Message', $exception->getMessage());
     }
 
     public function testBadRequestGetMessage()
     {
-        $response = new Response(400, ['Content-Type'=>'text/html'], '<html><body>Server HTML</body></html>');
+        $response = new Response(400, ['Content-Type' => 'text/html'], '<html><body>Server HTML</body></html>');
         $exception = HttpClientException::badRequest($response);
         $this->assertStringEndsWith('<html><body>Server HTML</body></html>', $exception->getMessage());
     }
