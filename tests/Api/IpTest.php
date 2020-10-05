cd991d924360e6c890039e90f8fcae4954018bf3
Joseph Shanak <joe.shanak@infosecinstitute.com>
2020-10-05 09:40:24 -0500

    Fix assigning Ip

---
+++ tests/Api/IpTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (C) 2013 Mailgun
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+namespace Mailgun\Tests\Api;
+
+use GuzzleHttp\Psr7\Response;
+use Mailgun\Api\Ip;
+use Mailgun\Model\Ip\UpdateResponse;
+
+class IpTest extends TestCase
+{
+    protected function getApiClass()
+    {
+        return Ip::class;
+    }
+
+    public function testAssign()
+    {
+        $this->setRequestMethod('POST');
+        $this->setRequestUri('/v3/domains/example.com/ips');
+        $this->setRequestBody([
+            'ip' => '127.0.0.1',
+        ]);
+        $this->setHydrateClass(UpdateResponse::class);
+
+        $api = $this->getApiInstance();
+        $api->assign('example.com', '127.0.0.1');
+    }
+}
