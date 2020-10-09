2d8283ea29bc1c28a61d005361ade2022c19415f
Artem Bondarenko <abonda@rainpos.com>
2020-10-10 00:43:27 +0300

    domain tracking implementation

---
+++ src/Model/Domain/TrackingResponse.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (C) 2013 Mailgun
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+namespace Mailgun\Model\Domain;
+
+use Mailgun\Model\ApiResponse;
+
+/**
+ * @author Artem Bondarenko <artem@uartema.com>
+ */
+final class TrackingResponse implements ApiResponse
+{
+    private $click;
+    private $open;
+    private $unsubscribe;
+
+    public static function create(array $data): ?self
+    {
+        if (!isset($data['tracking'])) {
+            return null;
+        }
+
+        $trackingSettings = $data['tracking'];
+
+        $model = new self();
+        $model->click = ClickTracking::create($trackingSettings['click'] ?? []);
+        $model->open = OpenTracking::create($trackingSettings['click'] ?? []);
+        $model->unsubscribe = UnsubscribeTracking::create($trackingSettings['unsubscribe'] ?? []);
+
+        return $model;
+    }
+
+    private function __construct()
+    {
+    }
+
+    /**
+     * @return ClickTracking
+     */
+    public function getClick(): ClickTracking
+    {
+        return $this->click;
+    }
+
+    /**
+     * @return OpenTracking
+     */
+    public function getOpen(): OpenTracking
+    {
+        return $this->open;
+    }
+
+    /**
+     * @return UnsubscribeTracking
+     */
+    public function getUnsubscribe(): UnsubscribeTracking
+    {
+        return $this->unsubscribe;
+    }
+}
---
+++ tests/Model/Domain/TrackingResponseTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (C) 2013 Mailgun
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+namespace Mailgun\Tests\Model\Domain;
+
+use Mailgun\Model\Domain\ClickTracking;
+use Mailgun\Model\Domain\OpenTracking;
+use Mailgun\Model\Domain\TrackingResponse;
+use Mailgun\Model\Domain\UnsubscribeTracking;
+use Mailgun\Tests\Model\BaseModelTest;
+
+class TrackingResponseTest extends BaseModelTest
+{
+    public function testCreate()
+    {
+        $json =
+            <<<'JSON'
+{
+    "tracking": {
+        "click": {
+            "active": true
+        },
+        "open": {
+            "active": true
+        },
+        "unsubscribe": {
+            "active": true,
+            "html_footer": "<s>Test<\/s>",
+            "text_footer": "Test"
+        }
+    }
+}
+JSON;
+        $model = TrackingResponse::create(json_decode($json, true));
+        $this->assertNotEmpty($model->getClick());
+        $this->assertInstanceOf(ClickTracking::class, $model->getClick());
+        $this->assertTrue($model->getClick()->isActive());
+
+        $this->assertNotEmpty($model->getOpen());
+        $this->assertInstanceOf(OpenTracking::class, $model->getOpen());
+        $this->assertTrue($model->getOpen()->isActive());
+
+        $this->assertNotEmpty($model->getUnsubscribe());
+        $this->assertInstanceOf(UnsubscribeTracking::class, $model->getUnsubscribe());
+        $this->assertTrue($model->getUnsubscribe()->isActive());
+        $this->assertEquals('<s>Test</s>', $model->getUnsubscribe()->getHtmlFooter());
+        $this->assertEquals('Test', $model->getUnsubscribe()->getTextFooter());
+    }
+}
