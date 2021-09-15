73565a6372524de031c853150a1bda99467cdf4e
Pavlico <pawlikwasiluk@gmail.com>
2021-09-15 13:38:47 +0200

    php-cs-fixes

--- src/Api/MailingList.php
+++ src/Api/MailingList.php
@@ -57,11 +57,11 @@ class MailingList extends HttpApi
     /**
      * Creates a new mailing list on the current domain.
      *
-     * @param string $address          Address for the new mailing list
-     * @param string|null $name        Name for the new mailing list (optional)
-     * @param string|null $description Description for the new mailing list (optional)
-     * @param string $accessLevel      List access level, one of: readonly (default), members, everyone
-     * @param string $replyPreference  Set where replies should go: list (default) | sender (optional)
+     * @param string      $address         Address for the new mailing list
+     * @param string|null $name            Name for the new mailing list (optional)
+     * @param string|null $description     Description for the new mailing list (optional)
+     * @param string      $accessLevel     List access level, one of: readonly (default), members, everyone
+     * @param string      $replyPreference Set where replies should go: list (default) | sender (optional)
      *
      * @return CreateResponse
      *
