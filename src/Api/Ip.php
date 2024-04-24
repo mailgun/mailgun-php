6c3f924633dfd6e6265e959d235a25dde72a4d83
Oleksandr Mykhailenko <mihaskep@gmail.com>
2024-04-24 14:44:27 +0300

    De 1262 api i ps endpoint. In Progress (#901)
    * Add dependabot. Change php-cs-fixer
    
    * Add missing API Endpoints. In Progress
    
    * More endpoints

--- src/Api/Ip.php
+++ src/Api/Ip.php
@@ -11,7 +11,9 @@ declare(strict_types=1);
 
 namespace Mailgun\Api;
 
+use Exception;
 use Mailgun\Assert;
+use Mailgun\Model\Ip\AvailableIpsResponse;
 use Mailgun\Model\Ip\IndexResponse;
 use Mailgun\Model\Ip\ShowResponse;
 use Mailgun\Model\Ip\UpdateResponse;
@@ -27,10 +29,11 @@ class Ip extends HttpApi
 {
     /**
      * Returns a list of IPs.
-     * @param  bool|null                       $dedicated
-     * @param  array                           $requestHeaders
+     * @param bool|null $dedicated
+     * @param array $requestHeaders
      * @return IndexResponse|ResponseInterface
      * @throws ClientExceptionInterface
+     * @throws Exception
      */
     public function index(?bool $dedicated = null, array $requestHeaders = [])
     {
@@ -47,10 +50,11 @@ class Ip extends HttpApi
 
     /**
      * Returns a list of IPs assigned to a domain.
-     * @param  string                          $domain
-     * @param  array                           $requestHeaders
+     * @param string $domain
+     * @param array $requestHeaders
      * @return IndexResponse|ResponseInterface
      * @throws ClientExceptionInterface
+     * @throws Exception
      */
     public function domainIndex(string $domain, array $requestHeaders = [])
     {
@@ -63,10 +67,11 @@ class Ip extends HttpApi
 
     /**
      * Returns a single ip.
-     * @param  string                         $ip
-     * @param  array                          $requestHeaders
+     * @param string $ip
+     * @param array $requestHeaders
      * @return ShowResponse|ResponseInterface
      * @throws ClientExceptionInterface
+     * @throws Exception
      */
     public function show(string $ip, array $requestHeaders = [])
     {
@@ -79,11 +84,12 @@ class Ip extends HttpApi
 
     /**
      * Assign a dedicated IP to the domain specified.
-     * @param  string                           $domain
-     * @param  string                           $ip
-     * @param  array                            $requestHeaders
+     * @param string $domain
+     * @param string $ip
+     * @param array $requestHeaders
      * @return UpdateResponse|ResponseInterface
      * @throws ClientExceptionInterface
+     * @throws Exception
      */
     public function assign(string $domain, string $ip, array $requestHeaders = [])
     {
@@ -101,11 +107,12 @@ class Ip extends HttpApi
 
     /**
      * Unassign an IP from the domain specified.
-     * @param  string                           $domain
-     * @param  string                           $ip
-     * @param  array                            $requestHeaders
+     * @param string $domain
+     * @param string $ip
+     * @param array $requestHeaders
      * @return UpdateResponse|ResponseInterface
      * @throws ClientExceptionInterface
+     * @throws Exception
      */
     public function unassign(string $domain, string $ip, array $requestHeaders = [])
     {
@@ -113,6 +120,103 @@ class Ip extends HttpApi
         Assert::ip($ip);
 
         $response = $this->httpDelete(sprintf('/v3/domains/%s/ips/%s', $domain, $ip), [], $requestHeaders);
+
+        return $this->hydrateResponse($response, UpdateResponse::class);
+    }
+
+    /**
+     * Get all domains of an account where a specific IP is assigned
+     * @param string $ip
+     * @param int $limit
+     * @param int $skip
+     * @param string|null $search
+     * @param array $requestHeaders
+     * @return IndexResponse|ResponseInterface
+     * @throws ClientExceptionInterface
+     * @throws Exception
+     */
+    public function domainsByIp(string $ip, int $limit = 10, int $skip = 0, ?string $search = null, array $requestHeaders = [])
+    {
+        Assert::stringNotEmpty($ip);
+
+        $params = [
+            'limit' => $limit,
+            'skip' => $skip,
+        ];
+        if (null !== $search) {
+            $params['search'] = $search;
+        }
+
+        $response = $this->httpGet(sprintf('/v3/ips/%s/domains', $ip), $params, $requestHeaders);
+
+        return $this->hydrateResponse($response, IndexResponse::class);
+    }
+
+    /**
+     * Place account IP into a dedicated IP band
+     * @param string $ip
+     * @param array $requestHeaders
+     * @return UpdateResponse|ResponseInterface
+     * @throws ClientExceptionInterface
+     * @throws Exception
+     */
+    public function placeAccountIpToBand(string $ip, array $requestHeaders = [])
+    {
+        Assert::stringNotEmpty($ip);
+        Assert::ip($ip);
+
+        $payload = [
+            'ip_band' => $ip,
+        ];
+
+        $response = $this->httpPost(sprintf('/v3/ips/%s/ip_band', $ip), $payload, $requestHeaders);
+
+        return $this->hydrateResponse($response, UpdateResponse::class);
+    }
+
+    /**
+     * Return the number of IPs available to the account per its billing plan
+     * @param array $requestHeaders
+     * @return AvailableIpsResponse|ResponseInterface
+     * @throws ClientExceptionInterface
+     * @throws Exception
+     */
+    public function numberOfIps(array $requestHeaders = [])
+    {
+        $response = $this->httpGet('v3/ips/request/new', [], $requestHeaders);
+
+        return $this->hydrateResponse($response, AvailableIpsResponse::class);
+    }
+
+    /**
+     * Add a new dedicated IP to the account
+     * @param array $requestHeaders
+     * @return UpdateResponse|ResponseInterface
+     * @throws ClientExceptionInterface
+     * @throws Exception
+     */
+    public function addDedicatedIp(array $requestHeaders = [])
+    {
+        $response = $this->httpPost('v3/ips/request/new', [], $requestHeaders);
+
+        return $this->hydrateResponse($response, UpdateResponse::class);
+    }
+
+    /**
+     * Remove an IP from the domain pool, unlink a DIPP or remove the domain pool
+     * @param string $domain
+     * @param string $ip
+     * @param array $requestHeaders
+     * @return UpdateResponse|ResponseInterface
+     * @throws ClientExceptionInterface
+     * @throws Exception
+     */
+    public function removeIpOrUnlink(string $domain, string $ip, array $requestHeaders = [])
+    {
+        Assert::stringNotEmpty($domain);
+        Assert::ip($ip);
+
+        $response = $this->httpDelete(sprintf('/v3/domains/%s/pool/%s', $domain, $ip), [], $requestHeaders);
 
         return $this->hydrateResponse($response, UpdateResponse::class);
     }
